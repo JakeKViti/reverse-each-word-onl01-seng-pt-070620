6098bbd50aa47ccac9d8474554b6ab85491c3289
Jacob Viti <thevitij64@gmail.com>
2020-07-13 04:31:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
     array = sentence.split
     finalSentence = []
     array.collect do |word|
-    finalSentence.push(word.reverse)    
+    finalSentence.push(word.reverse)
   end
   return finalSentence
 end
