c5ab4ff9f438b5fd5a11e7ec8cf4a25f07b03281
Jacob Viti <thevitij64@gmail.com>
2020-07-13 04:33:15 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def reverse_each_word(sentence)
+    array = sentence.split
+    finalSentence = []
+    array.collect do |word|
+    finalSentence.push(word.reverse)
+  end
+  return finalSentence.join(" ")
+end
